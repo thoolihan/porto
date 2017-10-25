1bc94d976daa5a414b2fdf4f58a7aeacbb1a2ad6
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2017-10-25 12:57:56 -0400

    Simply cross_val_predict call and Remove unnecessary .as_matrix calls

--- 04-lr-tune.py
+++ 04-lr-tune.py
@@ -5,7 +5,7 @@ from lib.porto.feature_type import get_cat_features_idx
 from lib.scoring.gini import gini_normalized
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, Imputer
-from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA
 from sklearn.model_selection import cross_val_predict, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from datetime import datetime
@@ -19,35 +19,34 @@ X = train.drop(['target'], axis = 1)
 y = train.target
 cat_columns = get_cat_features_idx(X)
 
-logger.info("Making pipeline...")
+logger.info("Making GridSearchCV Pipeline...")
 pipe = Pipeline([('impute', Imputer(missing_values = -1)),
                  ('encode', OneHotEncoder(categorical_features=cat_columns, handle_unknown = 'ignore')),
-                 ('decompose', TruncatedSVD()),
                  ('to_dense', FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
+                 ('decompose', PCA()),
                  ('model', LogisticRegression())])
 param_grid = {
     'impute__strategy': ["most_frequent"],
-    'decompose__n_compontents': [45],
+    'decompose__n_components': [20],
     'model': [LogisticRegression()],
-    'model__C': [1.],
+    'model__C': [.5, .75, 1.],
     'model__n_jobs': [1]
 }
 
-logger.info("Finding best parameters with GridSearchCV...")
 model = GridSearchCV(pipe, param_grid, scoring = 'roc_auc')
 
 logger.info("Fitting model on X...")
-model.fit(X.as_matrix(), y)
+model.fit(X, y)
 logger.info("Best Params: {}".format(model.best_params_))
 
 logger.info("Predicting score (w/Cross-Val) on X...")
-results = cross_val_predict(model, X, y, method = 'predict_proba')[:, 1]
+results = cross_val_predict(model.best_estimator_, X, y, cv = 3, method = 'predict_proba')[:, 1]
 score = gini_normalized(y, results)
 logger.info("normalized gini score on training set is {}".format(score))
 
 logger.info("Loading and predicting on Test set...")
 test = load_file("test")
-test['target'] = model.predict_proba(test.as_matrix())[:, 1]
+test['target'] = model.predict_proba(test)[:, 1]
 write_submission_file(test, columns = ['target'], name = 'ohe-cv-pipe')
 
 logger.info("Finished with time {}".format(datetime.now() - start))
