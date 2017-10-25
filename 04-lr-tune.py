5d639589a0029c8b4a7821939276a8d99ebce63e
Tim Hoolihan <tim.hoolihan@dialogtech.com>
2017-10-25 10:53:33 -0400

    Switch to Imputer

--- 04-lr-tune.py
+++ 04-lr-tune.py
@@ -1,10 +1,10 @@
-from lib.data import load_file, convert_columns_to_int, make_missing_zero
+from lib.data import load_file
 from lib.submit import write_submission_file
 from lib.logger import get_logger
 from lib.porto.feature_type import get_cat_features_idx
 from lib.scoring.gini import gini_normalized
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer, OneHotEncoder
+from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, Imputer
 from sklearn.model_selection import cross_val_predict, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from datetime import datetime
@@ -16,20 +16,18 @@ logger.info("Loading training data into X and y...")
 train = load_file()
 X = train.drop(['target'], axis = 1)
 y = train.target
-
-logger.info("Bumping all values up 1, so missing is now zero...")
 cat_columns = get_cat_features_idx(X)
-X = make_missing_zero(X, cat_columns)
 
 logger.info("Making pipeline...")
-pipe = Pipeline([('encode', OneHotEncoder(categorical_features=cat_columns, handle_unknown = 'ignore')),
+pipe = Pipeline([('impute', Imputer(missing_values=-1))
+                 ('encode', OneHotEncoder(categorical_features=cat_columns, handle_unknown = 'ignore')),
                  ('to_dense', FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
                  ('model', LogisticRegression())])
 param_grid = {
+    'impute__strategy': ["most_frequent"],
     'model': [LogisticRegression()],
     'model__C': [1.],
-    'model__n_jobs': [4],
-    'model__solver': ['sag', 'saga'],
+    'model__n_jobs': [1]
 }
 
 logger.info("Finding best parameters with GridSearchCV...")
@@ -45,7 +43,7 @@ score = gini_normalized(y, results)
 logger.info("normalized gini score on training set is {}".format(score))
 
 logger.info("Loading and predicting on Test set...")
-test = make_missing_zero(load_file("test"), cat_columns)
+test = load_file("test")
 test['target'] = model.predict_proba(test.as_matrix())[:, 1]
 write_submission_file(test, columns = ['target'], name = 'ohe-cv-pipe')
 
