219b298050134fbf7a24ebd429552e13c7aac136
Tim Hoolihan (AWS ML GPU Box) <tim@hoolihan.net>
2017-10-25 19:48:18 +0000

    Keras running

--- 05-keras.py
+++ 05-keras.py
@@ -4,8 +4,9 @@ from lib.logger import get_logger
 from lib.config import get_config
 from lib.porto.feature_type import get_cat_features_idx
 from lib.scoring.gini import gini_normalized
+import pandas as pd
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, Imputer
+from sklearn.preprocessing import OneHotEncoder, Imputer, FunctionTransformer
 from sklearn.model_selection import train_test_split
 from datetime import datetime
 from keras.models import Sequential
@@ -23,7 +24,8 @@ cat_columns = get_cat_features_idx(X)
 
 logger.info("Preprocessing Data (Impute, Encode)...")
 pipe = Pipeline([('impute', Imputer(missing_values = -1, strategy = "most_frequent")),
-                 ('encode', OneHotEncoder(categorical_features=cat_columns, handle_unknown = 'ignore'))])
+                 ('encode', OneHotEncoder(categorical_features=cat_columns, handle_unknown = 'ignore')),
+                 ('dense', FunctionTransformer(lambda x: x.todense(), accept_sparse = True))])
 
 pipe.fit(X,y)
 X = pipe.transform(X)
@@ -46,13 +48,15 @@ logger.info("Fitting model on X_train...")
 model.fit(X_train, y_train, epochs = cfg["epochs"], batch_size = cfg["batch_size"])
 
 logger.info("Predicting on X_val...")
-results = model.predict_proba(X_val)[:, 1]
+results = model.predict_proba(X_val)
 score = gini_normalized(y_val, results)
 logger.info("normalized gini score on validation set is {}".format(score))
 
 logger.info("Loading and predicting on Test set...")
 test = pipe.transform(load_file("test"))
-test['target'] = model.predict_proba(test)[:, 1]
+results = model.predict_proba(test)
+test = pd.DataFrame(test)
+test['target'] = results
 write_submission_file(test, columns = ['target'], name = 'ohe-cv-pipe')
 
 logger.info("Finished with time {}".format(datetime.now() - start))
