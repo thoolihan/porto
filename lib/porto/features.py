ebb01b427def3b0997de0c08f5c9d044243da843
Tim Hoolihan <tim@hoolihan.net>
2017-11-28 15:46:22 -0500

    Allow get_cat... to return col names instead of index

--- lib/porto/features.py
+++ lib/porto/features.py
@@ -2,8 +2,11 @@ from lib.logger import get_logger
 
 logger = get_logger()
 
-def drop_cols(df):
+def drop_cols(df, names = False):
     drop_cols = ["ps_calc_{:02d}".format(n) for n in range(2, 15)]
     drop_cols.append("ps_ind_12_bin")
     drop_idx = [i for i, name in enumerate(df.columns.values) if name in drop_cols]
-    return(drop_idx)
+    if names:
+        return drop_cols
+    else:
+        return(drop_idx)
